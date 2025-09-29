18ca43eaa07d5f4f9b9b94b05ce8e4b8d4a63c26
alone_coder <blackdevil190402@gmail.com>
2025-09-29 20:46:41 +0530

    CRUD operation to api

--- server/storage/api/url.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_books
-
-urlpatterns = [
-    path( 'books/', get_books , name='get_books')
-]
--- server/storage/api/views.py
+++ server/storage/api/views.py
@@ -21,3 +21,21 @@ def create_book(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT','DELETE'])
+
+def book_detail(request, pk):
+    try :
+        book = Book.objects.get(pk = pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book,data =request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
